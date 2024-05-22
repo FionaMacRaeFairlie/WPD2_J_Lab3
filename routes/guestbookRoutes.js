4461580fc4908d7271f0a5f0cb9e05bc0269131f
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2024-05-22 15:13:50 +0100

    WPD2 Jinan Lab 2

--- routes/guestbookRoutes.js
+++ routes/guestbookRoutes.js
@@ -1,20 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/guestbookControllers.js');
+const path = require("path");
+const public = path.join(__dirname, "../public");
+router.use(express.static(public));
+
+const controller = require("../controllers/guestbookControllers.js");
+
 router.get("/", controller.landing_page);
-router.get('/guestbook', controller.entries_list);
+router.get("/guestbook",controller.entries_list);
 router.get('/new', controller.new_entry);
-router.get('/about', function(req, res) {
-    res.redirect('/about.html');
-})
-router.use(function(req, res) {
-    res.status(404);
-    res.type('text/plain');
-    res.send('404 Not found.');
-})
-router.use(function(err, req, res, next) {
-    res.status(500);
-    res.type('text/plain');
-    res.send('Internal Server Error.');
-})
+router.get("/about", function (req, res) {
+  res.sendFile(path.join(public, "about.html"));
+});
+router.get('/peter', controller.peters_entries);
+router.use(function (req, res) {
+  res.status(404);
+  res.type("text/plain");
+  res.send("404 Not found.");
+});
+
+router.use(function (err, req, res, next) {
+  res.status(500);
+  res.type("text/plain");
+  res.send("Internal Server Error.");
+});
+
 module.exports = router;
