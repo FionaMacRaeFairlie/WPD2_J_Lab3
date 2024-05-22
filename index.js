f21723e89a6d27a8cc796017c9384246ce0041c8
FionaMacRaeFairlie <88429943+FionaMacRaeFairlie@users.noreply.github.com>
2024-05-22 15:45:43 +0100

    WPD2 Jinan Lab3

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.use(express.urlencoded({ extended: false }));
 const router = require('./routes/guestbookRoutes');
 app.use('/', router); 
 
-
 app.listen(3000, () => {
     console.log('Server started on port 3000. Ctrl^c to quit.');
 })
